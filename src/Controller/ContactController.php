d170748296845927af3a00a50958121910b4fcdd
Spreding <spreding.games@gmail.com>
2021-09-23 02:11:43 +0200

    page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,20 +1,42 @@
 <?php
 
+
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/Contact", name="Contact")
+     * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('spredingdesign@gmail.com')
+                ->subject('vous avez reçu un email de '.$contactFormData['fullName'])
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['message'],
+                    'text/plain');
+            $mailer->send($message);
+            $this->addFlash('success', 'Vore message a été envoyé');
+            return $this->redirectToRoute('contact');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
+    
 }
