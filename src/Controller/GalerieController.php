3d9f06a719b7504aed08167099e208a32dcff4e1
Tanguy <spreding.games@gmail.com>
2022-06-26 17:24:32 +0200

    forget dd

--- src/Controller/GalerieController.php
+++ src/Controller/GalerieController.php
@@ -33,7 +33,6 @@ class GalerieController extends AbstractController
     public function Maquillage($slug): Response
     {
         $Categorie = $this->entityManager->getRepository(MaquillageProduct::class)->findOneBy(['slug' => $slug]);
-        dd($Categorie);
         if (!$Categorie) {
             return $this->redirectToRoute("Maquillage");
 //            throw $this->createNotFoundException('Error 404');
